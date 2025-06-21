89511f666187a341a22641e6350ba5555ff35fd6
ski-mask-the-slump-god <drakonfyre212@gmail.com>
2025-06-21 18:40:39 -0400

    might have to go back one

--- backend.py
+++ backend.py
@@ -6,9 +6,12 @@ from pydantic import BaseModel
 
 client = genai.Client(api_key="AIzaSyDAVXbT9QeZp4g3kxzVunP7Xo6koqJZk5Q")
 spare_key = 'AIzaSyAsooTvjTuhVcBG6x_7BIAl0ix9w0NTdVU'
-image_path = "images/boxes/4.png"
+image_path = "images/boxes/0.png"
 my_file = client.files.upload(file=image_path)
 img = cv2.imread(image_path)
+parking_spaces = []
+results = []
+
 
 class Parking(BaseModel):
     box_2d: list[int]
@@ -18,7 +21,7 @@ def clean_results(results):
     # Example cleaning logic to ensure results is a valid JSON string
     return results.strip()
 
-prompt2 = " Detect the 2d bounding boxes of objects in image. Return just box_2d and labels, no additional text."
+prompt2 = "Detect the 2D bounding boxes of objects in the image. Return just box_2d and labels, no additional text."
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=[my_file, prompt2],
@@ -29,9 +32,7 @@ response = client.models.generate_content(
 )
 json_object = json.loads(response.text)
 
-i = 5
 height, width = img.shape[:2]
-parking_spaces = []
 
 for idx, item in enumerate(json_object):
 
@@ -39,21 +40,29 @@ for idx, item in enumerate(json_object):
 
    # Scale normalized box coordinates (0–1000) to image dimensions
    y1, x1, y2, x2 = item["box_2d"]
-   xcoord = (int(x1) + int(x2)) // 2
-   ycoord = (int(y1) + int(y2)) // 2
    abs_y1 = int(y1/1000 * height)
    abs_x1 = int(x1/1000 * width)
    abs_y2 = int(y2/1000 * height)
    abs_x2 = int(x2/1000 * width)
+   circle_x = (abs_x1 + abs_x2) // 2
+   circle_y = (abs_y1 + abs_y2) // 2
    top_left = (abs_x1, abs_y1)
    bottom_right = (abs_x2, abs_y2)
    parking_spaces.append([abs_x1, abs_y1, abs_x2, abs_y2])
-   spot_coords = (xcoord, ycoord)
+   if item["label"] != "car":
+        results.append([circle_x, circle_y])
+        #cv2.circle(img, center=(circle_x, circle_y), radius=20, color=(0, 255, 0), thickness=-1)
    cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
 #    cv2.circle(img, spot_coords, radius=10, color=(0, 255, 0), thickness=-1)
 
-new_img = "box-lot4.jpg"   
+new_img = "box-lot0-0.jpg"   
 cv2.imwrite(new_img, img)
+# img3 = "box-lot4-4-8.jpg"
+# for i in results:
+#     x, y = i
+#     cv2.circle(img, center=(x, y), radius=5, color=(0, 255, 0), thickness=-1)
+# cv2.imwrite(img3, img)  #
+
 
 # results = {"occupied": [], "empty": []}
 # img2 = cv2.imread(new_img)
@@ -75,5 +84,3 @@ cv2.imwrite(new_img, img)
 #         results["occupied"].append(i + 1)
 #     else:
 #         results["empty"].append(i + 1)
-
-
