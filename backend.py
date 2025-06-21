f6475b0ebfca08166de9f630b9f58fa3a80be299
ski-mask-the-slump-god <drakonfyre212@gmail.com>
2025-06-21 14:02:36 -0400

    HOLY SHIT IM SUCH A GENIUS ONG IM GONNA GOOOOOOOOON RN GEMINI CAN SEEEE

--- backend.py
+++ backend.py
@@ -2,26 +2,14 @@ from google import genai
 import json
 import re
 import cv2
+
 client = genai.Client(api_key="AIzaSyDAVXbT9QeZp4g3kxzVunP7Xo6koqJZk5Q")
+spare_key = 'AIzaSyAsooTvjTuhVcBG6x_7BIAl0ix9w0NTdVU'
+image_path = "images/boxes/0.png"
+my_file = client.files.upload(file=image_path)
+img = cv2.imread(image_path)
 
-my_file = client.files.upload(file="images/boxes/0.png")
-img = cv2.imread("images/boxes/0.png")
-prompt = """
 
-"Analyze the provided aerial image of a parking lot with a central driving lane and perform the following tasks:
-
-    Identify an open space in the driving lane where a car could potentially fit. The driving lane is the central asphalt strip running through the parking lot. The open space should be large enough for a car and not occupied by another vehicle or any obstacle. Determine the coordinates of the center of this open space.
-    Identify any open parking spaces that are not occupied by a vehicle. Parking spaces are designated spots marked with white lines and may be outlined with colored bounding boxes. Parking spaces outlined in red are taken, parking spaces outlined in blue are vacant and can be used. If there are multiple open parking spaces, select the first one from the top of the image. Determine the coordinates of the center of this open parking space.
-
-Provide the results in the following format, with each item on a separate line:
-    car: (x, y)
-parking spot: (x, y)
-
-Where (x, y) are the pixel coordinates of the center of the respective space, with (0,0) being the top-left corner of the image.
-
-If no open space is found in the driving lane, use 'car: none'. If no parking spaces are available, use 'parking spot: none'."
-
-"""
 def clean_results(results):
     # Example cleaning logic to ensure results is a valid JSON string
     return results.strip()
@@ -57,6 +45,8 @@ json_object = json.loads(json_str)
 
 # cln_results = json.loads(clean_results(response.txt))
 i = 5
+height, width = img.shape[:2]
+
 for idx, item in enumerate(json_object):
 
    # By default, gemini model return output with y coordinates first.
@@ -65,13 +55,17 @@ for idx, item in enumerate(json_object):
    y1, x1, y2, x2 = item["box_2d"]
    xcoord = (int(x1) + int(x2)) // 2
    ycoord = (int(y1) + int(y2)) // 2
-   top_left = (x1, y1)
-   bottom_right = (x2, y2)
+   abs_y1 = int(y1/1000 * height)
+   abs_x1 = int(x1/1000 * width)
+   abs_y2 = int(y2/1000 * height)
+   abs_x2 = int(x2/1000 * width)
+   top_left = (abs_x1, abs_y1)
+   bottom_right = (abs_x2, abs_y2)
    spot_coords = (xcoord, ycoord)
    cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
 #    cv2.circle(img, spot_coords, radius=10, color=(0, 255, 0), thickness=-1)
 
    
-cv2.imwrite("annotated_lot10.jpg", img)
+cv2.imwrite("annotated_lot12.jpg", img)
 
 
